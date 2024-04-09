23f88814eb41c706104fd56ea0a99924e9467007
Grace Nieboer <grace.nieboer@outlook.com>
2024-04-08 21:14:49 -0400

    Landing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,7 +1,8 @@
 const Landing = () => {
     return (
-        <div>
-            ENTER
+        <div className="w-screen h-screen relative">
+            <img className="object-cover" src="https://unsplash.com/photos/stainless-steel-chairs-in-room-soJBvazDKL0" />
+            <div className="absolute top-1/2 left-1/2">ENTER</div>
         </div>
     )
 };
