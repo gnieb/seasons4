64e382e9af2689e5f9b589c4b78be47428fe2d58
Grace Nieboer <grace.nieboer@outlook.com>
2024-04-11 17:03:51 -0400

    import AnimatePresence and wrap routes

--- src/components/AnimatedRoutes.js
+++ src/components/AnimatedRoutes.js
@@ -1,14 +1,18 @@
 import { Route, Routes,useLocation } from "react-router-dom";
+// import {} from "framer-motion/dist/framer-motion";
+import { AnimatePresence } from "framer-motion";
 
 const AnimatedRoutes = () => {
 
     const location = useLocation();
 
     return (
-        <Routes location={location} key={location.pathname}>
-          <Route path="/" element={<Landing />} />
-          <Route path="main" element={<Home />} />
-        </Routes>
+        <AnimatePresence>
+            <Routes location={location} key={location.pathname}>
+                <Route path="/" element={<Landing />} />
+                <Route path="main" element={<Home />} />
+            </Routes>
+        </AnimatePresence>
     )
 };
 
