ee40ceb7d9742d3d1e2e3be73497f2cef1859fb1
Grace Nieboer <grace.nieboer@outlook.com>
2024-04-15 21:56:25 -0400

    Testimonials component styling

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,13 +1,13 @@
 const Testimonials = () => {
     return (
-        <div className="w-screen h-1/2 bg-white opacity-70 flex items-center justify-center text-black font-black">
+        <div className="text-white w-screen h-1/2 bg-white opacity-75 flex items-center justify-center grayscale bg-[url('https://images.unsplash.com/photo-1481833761820-0509d3217039?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGJvY2ElMjBzdHJpbmclMjByZXN0YXVyYW50JTIwbGlnaHRzfGVufDB8fDB8fHww')]">
            <div className="">
-                <div className="text-3xl">Testimonials</div>
-                <div>Food was great and very flavorful. Thanks to Samuel he made our last minute office party happen ! Over all great experience. Big thanks !!</div>
-                <div className="flex justify-center">
-                    <div className="w-10">←</div>
-                    <div className="w-30">Eric Mason</div>
-                    <div className="w-10">→</div>
+                <div className="text-4xl py-10 font-black">Testimonials</div>
+                <div className="italic py-10 w-1/2 m-auto">Food was great and very flavorful. Thanks to Samuel he made our last minute office party happen ! Over all great experience. Big thanks !!</div>
+                <div className="flex justify-center py-10">
+                    <div className="w-10 font-black">←</div>
+                    <div className="w-30 italic font-black">Eric Mason</div>
+                    <div className="w-10 font-black">→</div>
                 </div>
             </div> 
         </div>
