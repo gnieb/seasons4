bf33bd3a38ffda75b16490eaebccbc55b1fb7e89
Grace Nieboer <grace.nieboer@outlook.com>
2024-04-15 21:46:34 -0400

    background image and homepage styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,8 @@ const Home = ()=> {
         className="h-screen w-screen bg-stone-900"
         >
             <TopMenu />
-            <div className="text-white font-black">HOME!!!!!</div>
+
+            <div className="text-white font-black h-1/3 bg-center bg-white bg-cover bg-opacity-50 opacity-65 bg-no-repeat bg-[url('https://images.unsplash.com/photo-1628294897055-a9bfca0f47ba?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDExfHx8ZW58MHx8fHx8')]">HOME!!!!!</div>
             <Testimonials />
         </motion.div>
     )
