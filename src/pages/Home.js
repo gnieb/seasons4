3e61994d9ddb1e08e673ee88b2fe0a6759504e0c
Grace Nieboer <grace.nieboer@outlook.com>
2024-04-16 21:31:53 -0400

    hover animation on Catering services

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,8 +13,8 @@ const Home = ()=> {
             <div className="flex items-center justify-center py-16 text-black bg-center bg-white bg-cover bg-opacity-50 opacity-65 bg-no-repeat bg-[url('https://images.unsplash.com/photo-1628294897055-a9bfca0f47ba?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDExfHx8ZW58MHx8fHx8')]">
                 <div className="">   
                     <div className="text-3xl text-black py-5 ">Catering Services</div>
-                    <div className="italic py-5">We serve locally sourced custom catering.</div>
-                    <div><NavLink className="bg-black italic text-white px-6 p-3">SEE MORE</NavLink></div>
+                    <div className="italic py-5 font-black">We serve locally sourced custom catering.</div>
+                    <div><NavLink className="hover:bg-stone-900 bg-black italic text-white px-6 p-3">SEE MORE</NavLink></div>
                 </div> 
             </div>
             <Testimonials />
