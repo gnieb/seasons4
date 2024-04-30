6b9228a3a99c0c33dc2cdba6238800c227c11926
Grace Nieboer <grace.nieboer@outlook.com>
2024-04-30 18:20:35 -0400

    add opcacity hover to one square... too dark. restyle paragraph divs

--- src/components/ReviewGrid.js
+++ src/components/ReviewGrid.js
@@ -1,8 +1,8 @@
 const ReviewGrid = () => {
     return (
-        <div class="m-20 mx-32 grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-4">
+        <div class="m-20 lg:mx-32 grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 text-md md:text-sm">
                
-                <div>
+                <div className="hover:opacity-50 ">
                     <img className="object-cover object-center w-full h-48 max-w-full " src="https://images.unsplash.com/photo-1543392883-914cfa6f9ed4?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
                 </div>
                 <div>
@@ -12,13 +12,13 @@ const ReviewGrid = () => {
                     <img className="object-cover object-center w-full h-48 max-w-full " src="https://images.unsplash.com/photo-1484156818044-c040038b0719?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
                 </div>
                 <div className="bg-stone-500 p-3">
-                    <p className="text-left font-serif font-bold">Food was great and very flavorful. Thanks to Samuel, he made our last minute office party happen ! Over all great experience.</p>
+                    <p className="text-left font-serif font-bold w-full ">Food was great and very flavorful. Thanks to Samuel, he made our last minute office party happen ! Over all great experience.</p>
                 </div>
                 <div>
                     <img className="object-cover object-center w-full h-48 max-w-full " src="https://images.unsplash.com/photo-1519671282429-b44660ead0a7?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
                 </div>
                 <div className="bg-stone-400 p-3">
-                    <p className="text-left font-serif font-bold">Seasons4 Catering made our event such a memorable experience! We enjoyed every hand-crafted dish! </p>
+                    <p className="text-left font-serif font-bold w-full ">Seasons4 Catering made our event such a memorable experience! We enjoyed every hand-crafted dish! </p>
                 </div>
                 <div>
                     <img className="object-cover object-center w-full h-48 max-w-full " src="https://images.unsplash.com/photo-1616864765695-fc15e791440c?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
@@ -33,7 +33,7 @@ const ReviewGrid = () => {
                     <img className="object-cover object-center w-full h-48 max-w-full " src="https://images.unsplash.com/photo-1566802516196-1bb541e324e2?q=80&w=2835&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
                 </div>
                 <div className="bg-stone-100 p-3">
-                    <p className="text-left font-serif font-bold">Lorem ipsum etc bleh efo theya qezak shi haluud quisak hadarrac</p>
+                    <p className="text-left font-serif font-bold w-full">Lorem ipsum etc bleh efo theya qezak shi haluud quisak hadarrac</p>
                 </div>
                 <div>
                     <img className="object-cover object-center w-full h-48 max-w-full " src="https://images.unsplash.com/photo-1579178404017-47844de5ca6a?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
