24d0aeb55a4cf4fa65a3970d630417ba1bf71a30
Grace Nieboer <grace.nieboer@outlook.com>
2024-04-29 20:43:09 -0400

    create Review Grid component

---
+++ src/components/ReviewGrid.js
@@ -0,0 +1,16 @@
+const ReviewGrid = () => {
+    return (
+        <div class="grid grid-cols-4 gap-4">
+            <div>01</div>
+            <div>01</div>
+            <div>01</div>
+            <div>01</div>
+            <div>01</div>
+            <div>01</div>
+            <div>01</div>
+            <div>09</div>
+        </div>
+    )
+};
+
+export default ReviewGrid;
